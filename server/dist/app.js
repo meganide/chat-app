627201c9e143cca8bacb578df0dd799e5b2f2128
Meganide <renashassan@hotmail.com>
2022-12-15 08:43:03 +0100

    added logs

--- server/dist/app.js
+++ server/dist/app.js
@@ -8,6 +8,9 @@ import { config } from './config.js';
 import { requireAuth } from './middlewares/auth.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+console.log('filename', __filename)
+console.log('dirname', __dirname)
 const app = express();
 app.use(express.json());
 app.use(cookieSession({
