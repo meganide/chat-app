86169847ecdad3c634220e6c222acf6e40068956
Meganide <renashassan@hotmail.com>
2022-12-16 18:39:16 +0100

    added aws settings for prod

--- server/dist/services/database.js
+++ server/dist/services/database.js
@@ -1,9 +1,21 @@
 import { config } from '../config.js';
 import mysql from 'mysql2';
-const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: config.DB_PASSWORD,
-    database: 'chatify',
-});
+let dbOptions;
+if (process.env.NODE_ENV === 'DEVELOPMENT') {
+    dbOptions = {
+        host: 'localhost',
+        user: 'root',
+        password: config.DB_PASSWORD,
+        database: 'chatify',
+    };
+}
+else {
+    dbOptions = {
+        host: config.DB_AWS_HOST,
+        user: 'admin',
+        password: config.DB_AWS_PASSWORD,
+        database: 'chatify',
+    };
+}
+const db = mysql.createConnection(dbOptions);
 export { db };
