1cb0128a21caedecb8dee18f8b61c9535254d764
Meganide <renashassan@hotmail.com>
2023-01-02 21:46:29 +0100

    add new channel screen responsive for mobile + desktop

--- client/src/components/ChatSidebar/AddNewChannel/AddNewChannel.tsx
+++ client/src/components/ChatSidebar/AddNewChannel/AddNewChannel.tsx
@@ -2,19 +2,36 @@ import { useContext } from 'react';
 
 import { ISidebarContext, SidebarContext } from '../../../contexts/SidebarContext';
 import './addnewchannel.css';
+import CloseIcon from '@mui/icons-material/Close';
 
 function AddNewChannel() {
-  const { showAddNewChannel } = useContext(SidebarContext) as ISidebarContext;
+  const { showAddNewChannel, setShowAddNewChannel } = useContext(SidebarContext) as ISidebarContext;
 
   if (showAddNewChannel) {
-    return <div className="add-channel">
-      <p>New channel</p>
-      <form className='add-channel__form' action="">
-        <input type="text" name="channel-name" id="channel-name" placeholder='Channel name' required />
-        <textarea name="channel-desc" id="channel-desc" placeholder='Channel Description' required ></textarea>
-        <input className='add-channel__submit' type="submit" value="Save" />
-      </form>
-    </div>;
+    return (
+      <div className="add-channel">
+        <CloseIcon className="chat-sidebar__close-icon add-channel__close" onClick={() => setShowAddNewChannel(false)} />
+        <p>New channel</p>
+        <form className="add-channel__form" action="">
+          <input
+            type="text"
+            name="channel-name"
+            id="channel-name"
+            placeholder="Channel name"
+            required
+            maxLength={20}
+          />
+          <textarea
+            name="channel-desc"
+            id="channel-desc"
+            placeholder="Channel Description"
+            required
+            maxLength={100}
+          ></textarea>
+          <input className="add-channel__submit" type="submit" value="Save" />
+        </form>
+      </div>
+    );
   } else {
     return <></>;
   }
