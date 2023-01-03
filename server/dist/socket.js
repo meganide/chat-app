75d7efd219af49f18e3bc27ded968c6411a3d62b
Meganide <renashassan@hotmail.com>
2023-01-03 01:59:05 +0100

    user can send message

--- server/dist/socket.js
+++ server/dist/socket.js
@@ -2,6 +2,10 @@ import { io } from './server.js';
 function startSocket() {
     io.on('connection', (socket) => {
         console.log('a user connected with id', socket.id);
+        socket.on('message', (msg) => {
+            console.log(msg);
+            socket.broadcast.emit('message', msg);
+        });
     });
 }
 export { startSocket };
--- server/src/socket.ts
+++ server/src/socket.ts
@@ -1,8 +1,21 @@
 import { io } from './server.js';
 
+interface iMsg {
+  displayName: string,
+  date: string,
+  img: string,
+  message: string,
+}
+
 function startSocket() {
   io.on('connection', (socket) => {
     console.log('a user connected with id', socket.id);
+
+
+    socket.on('message', (msg: iMsg) => {
+      console.log(msg);
+      socket.broadcast.emit('message', msg)
+    });
   });
 }
 
